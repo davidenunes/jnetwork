c5b52d97927f3795a184eaea6851916619d131b4
Davide Nunes <davex.pt@gmail.com>
2013-01-31 02:12:14 +0000

    Copyright and Licence Details added to DynamicNetwork

--- src/main/java/org/bhave/network/impl/hash/dynamic/DynamicHashNetwork.java
+++ src/main/java/org/bhave/network/impl/hash/dynamic/DynamicHashNetwork.java
@@ -1,3 +1,26 @@
+/**
+ * Copyright 2013 Davide Nunes
+ * Authors : Davide Nunes <davex.pt@gmail.com>
+ * Website : http://davidenunes.com 
+ * 
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * This file is part of the b-have network library.
+ * 
+ * The b-have network library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The b-have network library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the b-have network library.  
+ * If not, see <http://www.gnu.org/licenses/gpl.html>.
+ */
 package org.bhave.network.impl.hash.dynamic;
 
 import java.util.Collection;
