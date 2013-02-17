3291a09796e43af8c5daa1299459eb98e30e0d66
Davide Nunes <davex.pt@gmail.com>
2013-02-17 22:48:46 +0000

    WSModel Interface updated

--- src/main/java/org/bhave/network/model/WSModel.java
+++ src/main/java/org/bhave/network/model/WSModel.java
@@ -25,11 +25,41 @@
 
 package org.bhave.network.model;
 
+import org.apache.commons.configuration.ConfigurationException;
+import org.bhave.network.api.Network;
+
 /**
+ * <p>
+ * Defines a network model that creates {@link Network} instances with
+ * small-world properties according to the algorithm of Watts & Strogatz. The
+ * model starts from a k-regular network where the nodes have a degree of
+ * <i>2d</i> (<i>d</i> is an argument provided to the model) and evolve from
+ * that re-wiring each link with a probability p.
+ * </p>
  * 
  * @author Davide Nunes
  * 
  */
 public interface WSModel extends NetworkModel {
+	/**
+	 * Configures this model to generate a small-world network with a given
+	 * number of nodes.
+	 * 
+	 * @param numNodes
+	 *            number of nodes in the generated network
+	 * @param d
+	 *            minimum degree for the initial seed network
+	 * @param p
+	 *            re-wiring probability 0<=p<1 this is used to generate a
+	 *            network between a k-regular network and a random network.
+	 * 
+	 * @param seed
+	 *            the seed to be used in the random number generator of this
+	 *            model
+	 * @throws ConfigurationException
+	 *             if the model is not correctly configured.
+	 */
+	void configure(int numNodes, int d, double p, long seed)
+			throws ConfigurationException;
 
 }
